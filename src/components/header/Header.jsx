18ed3781a7d409005daca283c312f99974744cc7
Muhammad Aurif <muhammadaurifsolangixwave@gmail.com>
2024-10-26 11:23:42 +0500

    navbar color changed and bugs fixing

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-      <nav class="navbar navbar-expand-lg bg-black sticky-top">
+      <nav class="navbar navbar-expand-lg sticky-top">
         <div class="container-fluid">
           <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
